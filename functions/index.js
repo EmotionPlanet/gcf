596b40ed027ce4189be764b98335e5153198fa0d
杉山希望 <rioc.sugiyama@gmail.com>
2018-05-27 06:03:49 +0900

    fix

--- functions/index.js
+++ functions/index.js
@@ -50,7 +50,7 @@ exports.gameManager = functions.database.ref('/rooms/{roomName}')
             const firstTargetUserId = room.users[Math.floor(Math.random() * room.users.length)].id
 
             let date = new Date()
-            date.setSeconds( (original.game_time * 1000) + date.getSeconds() + 2000);
+            date.setSeconds( (parseInt(original.game_time) * 1000) + date.getSeconds() + 2000);
             console.log(date, "date debug point")
             return admin.database().ref('rooms/' + context.params.roomName ).update(
               {
